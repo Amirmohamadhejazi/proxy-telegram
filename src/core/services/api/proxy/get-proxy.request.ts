953919153258566c4059dcf47919c18c04d09a97
Amirmohamad <Amirmohamadhejazi.9731@gmail.com>
2025-08-15 18:36:57 +0330

    fix: fetch url

--- src/core/services/api/proxy/get-proxy.request.ts
+++ src/core/services/api/proxy/get-proxy.request.ts
@@ -4,7 +4,7 @@ import TProxyRequestType from '@/core/types/api/proxy/proxy-request-type'
 
 const getProxyFetchFn = async () => {
     // const url = `https://mtpro.xyz/api/?type=mtproto&_=${Date.now()}`
-    const url = `https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/mtproto.json?${Date.now()}`
+    const url = `https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/mtproto.json?nocache=${Date.now()}`
     return axios.get<TProxyRequestType[]>(url)
 }
 
